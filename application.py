b9d1eaa5e83305b8400443e7a70d20e68db63abe
Indranil Saha <136557599+indranil046@users.noreply.github.com>
2024-04-20 15:54:26 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from flask import Flask, render_template, request
+# Get the directory path of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the directory containing the 'src' package to the Python path
+src_dir = os.path.join(current_dir, 'src')
+sys.path.append(src_dir)
+
+# Now you should be able to import modules from the 'src' package
+from pipline.prediction import CustomData, PredictPipeline
+
+
+
+
+application=Flask(__name__)
+
+app=application
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
+    
+    else:
+        data=CustomData(
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
+
+        results=round(pred[0],2)
+
+        return render_template('form.html',final_result=results)
+    
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
